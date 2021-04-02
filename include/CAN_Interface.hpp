4a54a347acf545557c7638ddf6e22bcbbd0073e2
a64070ho <harry.obrien@student.manchester.ac.uk>
2021-04-02 21:34:02 +0100

    Fix typo

--- include/CAN_Interface.hpp
+++ include/CAN_Interface.hpp
@@ -15,61 +15,61 @@
 class CAN_Interface
 {
 #ifndef UNIT_TEST
-    MCP2515 CAN;
+	MCP2515 CAN;
 #else
-    // Mock
-    MCP2515 CAN;
+	// Mock
+	MCP2515 CAN;
 #endif
 
-    /// echo request frame model to be subscribed to
-    Subject<Echo_Request_Packet> latest_echo_request_frame;
-    /// echo response frame model to be subscribed to
-    Subject<Echo_Response_Packet> latest_echo_response_frame;
+	/// echo request frame model to be subscribed to
+	Subject<Echo_Request_Packet> latest_echo_request_frame;
+	/// echo response frame model to be subscribed to
+	Subject<Echo_Response_Packet> latest_echo_response_frame;
 
 public:
-    /**
+	/**
      * Constructor. Assigns the CS pin for the CAN module
      * @param CS_pin pin number of the chip select pin used
      */
-    CAN_Interface(uint8_t CS_pin = 10);
+	CAN_Interface(uint8_t CS_pin = 10);
 
-    /**
+	/**
      * Initialise the connected CAN module
      * @return true on success, false otherwise
      */
-    bool init(CAN_SPEED bit_rate = CAN_500KBPS, CAN_CLOCK clock_speed = MCP_16MHZ);
+	bool init(CAN_SPEED bit_rate = CAN_500KBPS, CAN_CLOCK clock_speed = MCP_16MHZ);
 
-    /**
+	/**
      * Send a frame across the CAN bus
      * @param frame - Frame to send 
      * @return true on success, false otherwise
      */
-    bool send(can_frame *frame);
+	bool send(can_frame *frame);
 
-    /**
+	/**
      * Read the latest message from the CAN bus. This should be called regularly
      */
-    void read_latest_message();
+	void read_latest_message();
 
-    /**
+	/**
      * Get the 'echo request' model for observers to subscribe to
      * @return the Echo_Request_Packet subject
      */
-    Subject<Echo_Request_Packet> *get_request_packet_model();
+	Subject<Echo_Request_Packet> *get_request_packet_model();
 
-    /**
+	/**
      * Get the 'echo response' model for observers to subscribe to
      * @return the Echo_Response_Packet subject
      */
-    Subject<Echo_Response_Packet> *get_response_packet_model();
+	Subject<Echo_Response_Packet> *get_response_packet_model();
 
 private:
-    /**
+	/**
      * Parse a packet into its specialised packet (e.g. echo_request_packet) and assign it to a subject to notify relevant subscribers
      * @param frame A pointer to the frame to parse
      * @return true if packet parsed successfully, false otherwise
      */
-    bool parse_and_update(can_frame *frame);
+	bool parse_and_update(can_frame *frame);
 };
 
 #endif /* lib_CAN_Interface_CAN_Interface_hpp */
