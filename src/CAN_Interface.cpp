f3751de9686210bf6703815b7e29bb750a8caf2d
a64070ho <harry.obrien@student.manchester.ac.uk>
2021-04-02 21:30:46 +0100

    Add additional log messages

--- src/CAN_Interface.cpp
+++ src/CAN_Interface.cpp
@@ -4,7 +4,7 @@
  * Constructor. Assigns the CS pin for the CAN module
  */
 CAN_Interface::CAN_Interface(uint8_t CS_pin)
-    : CAN(CS_pin)
+	: CAN(CS_pin)
 {
 }
 
@@ -16,28 +16,28 @@ CAN_Interface::CAN_Interface(uint8_t CS_pin)
  */
 bool CAN_Interface::init(CAN_SPEED bit_rate, CAN_CLOCK clock_speed)
 {
-    // Reset the CAN controller
-    if (CAN.reset() != MCP2515::ERROR_OK)
-    {
-        LOG_ERR(F("CAN BUS failed to init"));
-        return false;
-    }
+	// Reset the CAN controller
+	if (CAN.reset() != MCP2515::ERROR_OK)
+	{
+		LOG_ERR(F("CAN BUS failed to init"));
+		return false;
+	}
 
-    // Set to normal mode
-    if (CAN.setNormalMode() != MCP2515::ERROR_OK)
-    {
-        LOG_ERR(F("Failed to set CAN controller to 'normal' mode"));
-        return false;
-    }
+	// Set to normal mode
+	if (CAN.setNormalMode() != MCP2515::ERROR_OK)
+	{
+		LOG_ERR(F("Failed to set CAN controller to 'normal' mode"));
+		return false;
+	}
 
-    // Set bitrate to
-    if (CAN.setBitrate(bit_rate, clock_speed) != MCP2515::ERROR_OK)
-    {
-        LOG_ERR(F("Failed to set bitrate and/or clock speed for CAN controller"));
-        return false;
-    }
+	// Set bitrate to
+	if (CAN.setBitrate(bit_rate, clock_speed) != MCP2515::ERROR_OK)
+	{
+		LOG_ERR(F("Failed to set bitrate and/or clock speed for CAN controller"));
+		return false;
+	}
 
-    return true;
+	return true;
 }
 
 /**
@@ -47,13 +47,13 @@ bool CAN_Interface::init(CAN_SPEED bit_rate, CAN_CLOCK clock_speed)
  */
 bool CAN_Interface::send(can_frame *frame)
 {
-    if (CAN.sendMessage(frame) != MCP2515::ERROR_OK)
-    {
-        LOG_WARN(F("Failed to send last CAN message"));
-        return false;
-    }
+	if (CAN.sendMessage(frame) != MCP2515::ERROR_OK)
+	{
+		LOG_WARN(F("Failed to send last CAN message"));
+		return false;
+	}
 
-    return true;
+	return true;
 }
 
 /**
@@ -61,11 +61,11 @@ bool CAN_Interface::send(can_frame *frame)
  */
 void CAN_Interface::read_latest_message()
 {
-    can_frame latest_frame;
-    while (CAN.readMessage(&latest_frame) == MCP2515::ERROR_OK)
-    {
-        parse_and_update(&latest_frame);
-    }
+	can_frame latest_frame;
+	while (CAN.readMessage(&latest_frame) == MCP2515::ERROR_OK)
+	{
+		parse_and_update(&latest_frame);
+	}
 }
 
 /**
@@ -74,7 +74,7 @@ void CAN_Interface::read_latest_message()
  */
 Subject<Echo_Request_Packet> *CAN_Interface::get_request_packet_model()
 {
-    return &latest_echo_request_frame;
+	return &latest_echo_request_frame;
 }
 
 /**
@@ -83,7 +83,7 @@ Subject<Echo_Request_Packet> *CAN_Interface::get_request_packet_model()
  */
 Subject<Echo_Response_Packet> *CAN_Interface::get_response_packet_model()
 {
-    return &latest_echo_response_frame;
+	return &latest_echo_response_frame;
 }
 
 /**
@@ -92,27 +92,27 @@ Subject<Echo_Response_Packet> *CAN_Interface::get_response_packet_model()
  */
 bool CAN_Interface::parse_and_update(can_frame *frame)
 {
-    // Specialise packet and inform subscribers
-    switch (frame->can_id)
-    {
-    case Packet_Priority::PRIORITY_ECHO_REQUEST:
-    {
-        Echo_Request_Packet echo_request_packet;
-        copy_frame(&echo_request_packet, frame);
-        latest_echo_request_frame = echo_request_packet;
-        return true;
-    }
+	// Specialise packet and inform subscribers
+	switch (frame->can_id)
+	{
+	case Packet_Priority::PRIORITY_ECHO_REQUEST:
+	{
+		Echo_Request_Packet echo_request_packet;
+		copy_frame(&echo_request_packet, frame);
+		latest_echo_request_frame = echo_request_packet;
+		return true;
+	}
 
-    case Packet_Priority::PRIORITY_ECHO_RESPONSE:
-    {
-        Echo_Response_Packet echo_response_packet;
-        copy_frame(&echo_response_packet, frame);
-        latest_echo_response_frame = echo_response_packet;
-        return true;
-    }
+	case Packet_Priority::PRIORITY_ECHO_RESPONSE:
+	{
+		Echo_Response_Packet echo_response_packet;
+		copy_frame(&echo_response_packet, frame);
+		latest_echo_response_frame = echo_response_packet;
+		return true;
+	}
 
-    default:
-        LOG_WARN("Packet with id ", frame->can_id, " and length ", frame->can_dlc, "not recognised!");
-        return false;
-    }
+	default:
+		LOG_WARN("Packet with id ", frame->can_id, " and length ", frame->can_dlc, "not recognised!");
+		return false;
+	}
 }
